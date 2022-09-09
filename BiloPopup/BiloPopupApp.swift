1e654d1176156838c792492e71a957b8c21a6dfd
Bilal Durnagöl <bilal.durnagol.tt@gmail.com>
2022-09-09 09:12:06 +0300

    Initial Commit

---
+++ BiloPopup/BiloPopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiloPopupApp.swift
+//  BiloPopup
+//
+//  Created by Bilal Durnagöl on 9.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BiloPopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiloPopup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BiloPopup
+//
+//  Created by Bilal Durnagöl on 9.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
